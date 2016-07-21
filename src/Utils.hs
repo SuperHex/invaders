1ef84ed4c209d6539f914c6422ea14fed4228961
Soei Wong <wang_ruihan@hotmail.com>
2016-07-21 23:26:20 +0800

    encoding/decoding tool

---
+++ src/Utils.hs
@@ -0,0 +1,11 @@
+module Utils where
+
+import           Data.Bits
+import           Data.Word (Word16, Word8)
+
+encode :: (Integral a0, Integral a1, Num b, Bits b) => a0 -> a1 -> b
+encode h l = fromIntegral h `shiftL` 8 .|. fromIntegral l
+
+decode :: Word16 -> (Word8, Word8)
+decode word = ( fromIntegral $ (word .&. 0xFF00) `shiftR` 8
+              , fromIntegral $ word .&. 0x00FF )
