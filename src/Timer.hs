1e017d0e259581ef9792d4eb4cd1fec7b6c47c83
Soei Wong <wang_ruihan@hotmail.com>
2016-08-06 21:43:51 +0800

    lift timer

--- src/Timer.hs
+++ src/Timer.hs
@@ -8,27 +8,32 @@ type Timer = MVar TimerState
 
 data TimerState = Ready | Timeout
 
-newTimer :: IO Timer
-newTimer = do
+newTimer :: (MonadIO m) => m Timer
+newTimer = liftIO $ do
   m <- newEmptyMVar
   putMVar m Ready
   return m
 
-timeout :: Timer -> Int -> IO ()
-timeout t n = void . forkIO . forever $ do
-  threadDelay n
-  swapMVar t Timeout
+delay :: (MonadIO m) => Int -> m ()
+delay = liftIO . threadDelay
 
-withTimer :: (MonadIO m) => Int -> m a -> m a -> m a
-withTimer n action interrupt = do
-  t <- liftIO newTimer
-  liftIO $ t `timeout` n
-  loop t
-  where loop timer = do
-          state <- liftIO $ readMVar timer
-          void $ case state of
-                   Ready   -> action
-                   Timeout -> do
-                     liftIO . void $ swapMVar timer Ready
-                     interrupt
-          loop timer
+startTimer :: (MonadIO m) => Timer -> Int -> m ()
+startTimer timer n = liftIO . void . forkIO . forever $ do
+  threadDelay n
+  swapMVar timer Timeout
+
+checkTimer :: (MonadIO m) => Timer -> m Bool
+checkTimer timer = do
+  stat <- liftIO . readMVar $ timer
+  case stat of
+    Ready   -> return False
+    Timeout -> return True
+
+resetTimer :: (MonadIO m) => Timer -> m ()
+resetTimer timer = liftIO . void $ swapMVar timer Ready
+
+registerTimer :: (MonadIO m) => Int -> m Timer
+registerTimer n = do
+  t <- newTimer
+  startTimer t n
+  return t
